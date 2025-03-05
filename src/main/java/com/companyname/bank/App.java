775ad27bd39191cbc241f0dff425fcd8b2819e66
Nari841 <ragulanarendra56@gmail.com>
2025-03-05 18:34:44 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "source code updated by Narendra!" );
-        System.out.println("created by narendra"P);
+        System.out.println("created by narendra");
     }
 }
